ed3779200770b7c07c8ec17e0b5a239493cd6537
esolCrusador <boris.sotsky@gmail.com>
2024-11-22 13:31:33 +0300

    Added multi workflows initialization.

--- src/Rx.Net.StateMachine.Persistance/StateMachineBootstrapper.cs
+++ src/Rx.Net.StateMachine.Persistance/StateMachineBootstrapper.cs
@@ -4,6 +4,7 @@ using Rx.Net.StateMachine.Persistance.Attributes;
 using Rx.Net.StateMachine.States;
 using Rx.Net.StateMachine.WorkflowFactories;
 using System;
+using System.Linq;
 using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -29,7 +30,7 @@ namespace Rx.Net.StateMachine.Persistance
         }
 
         public static IServiceCollection UseWorkflowSessionValue<TValue>(this IServiceCollection services)
-            where TValue: class
+            where TValue : class
         {
             services.AddScoped<WorkflowSessionValueProvider<TValue>>();
             services.AddScoped<TValue>(sp => sp.GetRequiredService<WorkflowSessionValueProvider<TValue>>().Value);
@@ -70,6 +71,35 @@ namespace Rx.Net.StateMachine.Persistance
                     services.AddSingleton(WorkflowRegistration.Create(oldWorkflow));
                 }
             }
+
+            return services;
+        }
+
+        public static IServiceCollection AddWorkflow(this IServiceCollection services, Type workflowType, string? workflowId = default)
+        {
+            services.AddScoped(workflowType);
+            services.AddScoped<IWorkflow>(sp => (IWorkflow)sp.GetRequiredService(workflowType));
+            services.AddSingleton(WorkflowRegistration.Create(workflowType, workflowId));
+
+            var oldVersionsAttribute = workflowType.GetCustomAttribute<OldWorkflowVersionsAttribute>();
+            if (oldVersionsAttribute != null)
+            {
+                foreach (var oldWorkflow in oldVersionsAttribute.OldWorkflowVersions)
+                {
+                    services.AddScoped(oldWorkflow);
+                    services.AddScoped(typeof(IWorkflow), sp => sp.GetRequiredService(oldWorkflow));
+                    services.AddSingleton(WorkflowRegistration.Create(oldWorkflow));
+                }
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddWorkflows(this IServiceCollection services, Assembly assembly)
+        {
+            var worklowTypes = assembly.GetTypes().Where(at => at.IsClass && !at.IsAbstract && at.GetInterface(nameof(IWorkflow)) != null);
+            foreach (var wft in worklowTypes)
+                AddWorkflow(services, wft);
 
             return services;
         }
