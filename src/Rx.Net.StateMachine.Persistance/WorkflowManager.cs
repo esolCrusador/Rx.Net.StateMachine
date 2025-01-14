8d5152ae420e5d321a0b5a04c96baf1f415248f8
esolCrusador <boris.sotsky@gmail.com>
2025-01-14 17:46:23 +0300

    Fixed retries issue

--- src/Rx.Net.StateMachine.Persistance/WorkflowManager.cs
+++ src/Rx.Net.StateMachine.Persistance/WorkflowManager.cs
@@ -44,8 +44,8 @@ namespace Rx.Net.StateMachine.Persistance
             _concurrencyRetry = Policy.WrapAsync(
                 Policy.Handle<ConcurrencyException>().RetryForeverAsync(ex => _logger.LogWarning(ex.Message)),
                 Policy.Handle<NotPersistedException>().WaitAndRetryForeverAsync(
-                    i => TimeSpan.FromMinutes(Math.Pow(10, i)),
-                    (ex, d) => _logger.LogWarning(ex.Message)
+                    i => TimeSpan.FromMilliseconds(10 * i + Math.Pow(10, i)),
+                    (ex, d) => _logger.LogWarning("Error: {Exception}. Waiting: {d}", ex.Message, d)
                 )
             );
         }
