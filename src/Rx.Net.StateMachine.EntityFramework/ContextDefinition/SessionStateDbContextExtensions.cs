7e8870b44187aee9a6704146849da4c7a6bbb4cb
esolCrusador <boris.sotsky@gmail.com>
2025-07-22 20:05:59 +0300

    added postgress concurrency option

--- src/Rx.Net.StateMachine.EntityFramework/ContextDefinition/SessionStateDbContextExtensions.cs
+++ src/Rx.Net.StateMachine.EntityFramework/ContextDefinition/SessionStateDbContextExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.Extensions.Options;
 using Rx.Net.StateMachine.EntityFramework.Tables;
 using Rx.Net.StateMachine.EntityFramework.Tests.Tables;
@@ -6,28 +7,32 @@ using System;
 
 namespace Rx.Net.StateMachine.EntityFramework.ContextDfinition
 {
-    public class SessionStateTableOptions
+    public class SessionStateTableOptions<TContext, TContextKey>
     {
         public string SessionStateTableName { get; set; } = "SessionStates";
         public string SessionEventAwaiterTableName { get; set; } = "SessionStateAwaiters";
+        public Func<string, string> EscapePropertyName { get; set; } = name => $"[{name}]";
+        public Action<EntityTypeBuilder<SessionStateTable<TContext, TContextKey>>> ConfigureSessionStateTable { get; set; } = _ => { };
     }
     public static class SessionStateDbContextExtensions
     {
-        public static void ConfigureSessions<TContext, TContextKey>(this ModelBuilder modelBuilder, Action<IOptions<SessionStateTableOptions>>? configure = null)
+        public static void ConfigureSessions<TContext, TContextKey>(this ModelBuilder modelBuilder, Action<SessionStateTableOptions<TContext, TContextKey>>? configure = null)
             where TContext : class
         {
-            var options = Options.Create(new SessionStateTableOptions());
+            var options = new SessionStateTableOptions<TContext, TContextKey>();
             configure?.Invoke(options);
 
             modelBuilder.Entity<SessionStateTable<TContext, TContextKey>>(builder =>
             {
-                builder.ToTable(options.Value.SessionStateTableName);
+                builder.ToTable(options.SessionStateTableName);
                 builder.HasIndex(i => new { i.Status, i.UpdatedAt, i.WorkflowId });
             });
+            options.ConfigureSessionStateTable(modelBuilder.Entity<SessionStateTable<TContext, TContextKey>>());
             modelBuilder.Entity<SessionEventAwaiterTable<TContext, TContextKey>>(builder =>
             {
-                builder.ToTable(options.Value.SessionEventAwaiterTableName);
-                builder.HasIndex(aw => new { aw.IsActive, aw.Identifier }).HasFilter("[IsActive] = 1");
+                builder.ToTable(options.SessionEventAwaiterTableName);
+                builder.HasIndex(aw => new { aw.IsActive, aw.Identifier })
+                    .HasFilter($"{options.EscapePropertyName(nameof(SessionEventAwaiterTable<TContext, TContextKey>.IsActive))} = 1");
             });
             modelBuilder.Entity<TContext>();
         }
